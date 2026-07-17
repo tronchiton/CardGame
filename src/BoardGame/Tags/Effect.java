b59c44b8be0cdb4b5775cd70b62074415683a7e8
tronchiton <danieltronchonipc@gmail.com>
2026-07-17 18:36:27 +0200

    necesito crear una clase write que permita escribir y referenciar iconos en cartas.
    Effect hecho, o empezado, necesito un tagloader.

---
+++ src/BoardGame/Tags/Effect.java
@@ -0,0 +1,35 @@
+package BoardGame.Tags;
+
+public class Effect {
+    Tag Trigger;
+
+    public enum Target{
+        AnyPlayer,
+        You,
+        Opponent,
+        Battle,
+    }
+    Target Target;
+
+    public enum effect{
+        Defense,
+        Attack
+    }
+    effect effect;
+
+    int number;
+
+    public Effect(Tag Trigger, Target Target, effect effect, int number ){
+        this.Trigger=Trigger;
+        this.Target=Target;
+        this.effect=effect;
+        this.number=number;
+    }
+    public Effect(String Trigger, Target Target, effect effect, int number ){
+        this.Trigger=new Tag(Trigger);
+        this.Target=Target;
+        this.effect=effect;
+        this.number=number;
+    }
+
+}
